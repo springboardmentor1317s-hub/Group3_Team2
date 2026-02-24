2955ddd33e470620b7dda354a5052a2afc35e443
YeshwanthrajG <yeshwanth1155@gmail.com>
2026-02-24 23:11:42 +0530

    Update Commit : Milestone 2

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,9 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+const { createEvent, getEvents } = require("../controllers/eventController");
 const { authMiddleware } = require("../middleware/authMiddleware");
 const { roleMiddleware } = require("../middleware/roleMiddleware");
 
 router.post(
   "/create-event",
   authMiddleware,
-  roleMiddleware("college-admin"),
-  createEventController
+  roleMiddleware(["college-admin"]),
+  // createEventController,
+  createEvent
 );
+
+router.get(
+  "/",
+  authMiddleware,
+  getEvents
+);
+
+module.exports = router;
