b563875128575aea7ca4663d32bd794c91b427f3
komal <komaldeore9371@gmail.com>
2026-03-24 23:48:03 +0530

    Saving my progress before checking team code

---
+++ AngularProject/First-App/frontend/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { EventOrganizerDashboard } from './event-organizer-dashboard/event-organizer-dashboard';
+
+export const routes: Routes = [
+  { path: '', component: EventOrganizerDashboard }
+];
